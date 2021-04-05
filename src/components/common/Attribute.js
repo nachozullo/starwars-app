66b69f1e584491c7a0d37f764dafadf201df4027
nacho <ignaciozullo@gmail.com>
2021-04-05 00:58:57 -0300

    Add color preview

--- src/components/common/Attribute.js
+++ src/components/common/Attribute.js
@@ -1,23 +1,45 @@
 import React from "react";
-import { CircularProgress, makeStyles } from "@material-ui/core";
+import { CircularProgress, makeStyles, Tooltip } from "@material-ui/core";
 import { useGetAll } from "../../hooks";
+import { isValidCSSColor } from "../../utils/utils";
 
 const useStyles = makeStyles(theme => ({
   field: {
     color: theme.palette.text.secondary,
     fontWeight: "600",
+    marginRight: 5,
   },
   fieldContainer: {
     margin: "20px 0",
   },
 }));
 
+const ColorValue = ({ value }) => {
+  return value.split(", ").map(color => (
+    <div key={color} style={{ display: "flex", alignItems: "center" }}>
+      <Tooltip title={isValidCSSColor(color) ? "" : "Color can't be displayed."}>
+        <div
+          style={{
+            width: 16,
+            height: 16,
+            borderRadius: 8,
+            backgroundColor: isValidCSSColor(color) ? color : "transparent",
+            margin: "0 5px",
+            border: `.5px ${isValidCSSColor(color) ? "solid" : "dashed"} grey`,
+          }}
+        />
+      </Tooltip>
+      <span>{color}</span>
+    </div>
+  ));
+};
+
 const Attribute = ({ field, value }) => {
   const styles = useStyles();
   return (
-    <div className={styles.fieldContainer}>
+    <div className={styles.fieldContainer} style={{ display: "flex" }}>
       <span className={styles.field}>{field}: </span>
-      <span>{value}</span>
+      {field.includes("COLOR") ? <ColorValue value={value} /> : <span>{value}</span>}
     </div>
   );
 };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,9 @@
 export const toCamelCase = str => `${str[0].toUpperCase()}${str.slice(1)}`;
 
 export const httpToHttps = url => url.replace(/^http:\/\//i, "https://");
+
+export const isValidCSSColor = strColor => {
+  const s = new Option().style;
+  s.color = strColor;
+  return s.color === strColor;
+};
